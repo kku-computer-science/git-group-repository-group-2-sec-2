20cf035c18e926e0990e107cd79be3d8ff6ebd8f
WasitpolPhanchanokkul <wasitpol.p@kkumail.com>
2025-02-11 20:14:25 +0700

    fast scraping api + fast fetch
    might be done

--- cpresearchV2/app/Http/Controllers/GoogleScholarController.php
+++ cpresearchV2/app/Http/Controllers/GoogleScholarController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\GoogleScholarScraper;
+use App\Models\Paper;
 
 class GoogleScholarController extends Controller
 {
@@ -20,6 +21,29 @@ class GoogleScholarController extends Controller
 
         if (isset($data['error'])) {
             return response()->json(['error' => $data['error']], 500);
+        }
+
+        // 🔹 บันทึก Paper ลงฐานข้อมูล
+        foreach ($data['papers'] as $paper) {
+            Paper::updateOrCreate(
+                ['paper_name' => $paper['paper']],
+                [
+                    'abstract' => null,
+                    // 'paper_type' => $paper['paper_type_detail'] ?? null, 
+                    'paper_type' => null,
+                    'paper_url' => $paper['paperUrl'],
+                    'paper_yearpub' => is_numeric($paper['year']) ? (int) $paper['year'] : null,
+                    'paper_citation' => is_numeric($paper['citations']) ? (int) $paper['citations'] : 0,
+                    'paper_sourcetitle' => $paper['paper_type_detail'] ?? null,
+                    'publication' => null,
+                    'paper_volume' => null,
+                    'paper_issue' => null,
+                    'paper_page' => null,
+                    'paper_doi' => null,
+                    'paper_funder' => null,
+                    'reference_number' => null
+                ]
+            );
         }
 
         return response()->json($data);
