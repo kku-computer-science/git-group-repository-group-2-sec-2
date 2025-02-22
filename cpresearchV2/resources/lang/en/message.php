b9952496ec27e2681b8aeeb4093ee3390a2c6371
WasitpolPhanchanokkul <wasitpol.p@kkumail.com>
2025-02-22 16:44:27 +0700

    manage lang detail
    setup for change the lang

--- cpresearchV2/resources/lang/en/message.php
+++ cpresearchV2/resources/lang/en/message.php
@@ -1,13 +1,42 @@
 <?php
 return [
-'Home' => 'Home',
-'Researchers' => 'Researchers',
-'ResearchProj' => 'Research Project',
-'ResearchGroup' => 'Research Group',
-'Report' => 'Reports',
-'details' =>'More details',
-'expertise' => 'Research interests',
-'publications' => 'Publications (In the Last 5 Years)',
-'education'=>'Education',
-'publications2' => 'Publications',
+// cpresearchV2\resources\views\layouts\layout.blade.php
+    'Home'                          => 'Home',
+    'Researchers'                   => 'Researchers',
+    'ResearchProj'                  => 'Research Project',
+    'ResearchGroup'                 => 'Research Group',
+    'Report'                        => 'Reports',
+    'expertise'                     => 'Research interests',
+    'education'                     => 'Education',
+    'publications2'                 => 'Publications',
+    'login'                         => 'Login',
+
+// cpresearchV2\resources\views\home.blade.php
+    'publications'                  => 'Publications (In the Last 5 Years)',
+    'reference'                     => 'Reference',
+    'before'                        => 'Before',
+    // 'report_total_articles'      => 'Report the total number of articles (5 years: cumulative)',
+    // 'number'                     => 'Number',
+
+// cpresearchV2\resources\views\research_proj.blade.php
+    'project_service_or_research'   => 'Academic Service/ Research Project',
+    'order'                         => 'No.',
+    'year'                          => 'Year',
+    'project_name'                  => 'Project Name',
+    'details'                       => 'More details',
+    'project_leader'                => 'Project Leader',
+    'status'                        => 'Status',
+    'research_fund_type'            => 'Research Fund Type',
+    'funding_agency'                => 'Funding Agency',
+    'responsible_department'        => 'Responsible Department',
+    'allocated_budget'              => 'Allocated Budget',
+    'project_duration'              => 'Project Duration',
+    'to'                            => 'to',
+    'closed'                        => 'Closed',
+    'currency'                      => 'Baht',
+
+
+
+    'welcome_message' => 'Welcome to the research data management system of the Department of Computer Science',
+
 ];
--- cpresearchV2/resources/views/dashboards/users/layouts/user-dash-layout.blade.php
+++ cpresearchV2/resources/views/dashboards/users/layouts/user-dash-layout.blade.php
@@ -77,6 +77,24 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             <input type="text" class="form-control">
                         </div>
                     </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            <span
+                                class="flag-icon flag-icon-{{Config::get('languages')[App::getLocale()]['flag-icon']}}"></span>
+                            {{ Config::get('languages')[App::getLocale()]['display'] }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            @foreach (Config::get('languages') as $lang => $language)
+                            @if ($lang != App::getLocale())
+                            <a class="dropdown-item" href="{{ route('langswitch', $lang) }}"><span
+                                    class="flag-icon flag-icon-{{$language['flag-icon']}}"></span>
+                                {{$language['display']}}</a>
+                            @endif
+                            @endforeach
+                        </div>
+                    </li>
+                    
                     <li class="nav-item">
                         <form class="search-form" action="#">
                             <i class="icon-search"></i>
