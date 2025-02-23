2635d927d8be30827568579548c900c74502a82d
TharadolWichai <tharadol.w@kkumail.com>
2025-02-23 12:44:19 +0700

    translate chinese

--- cpresearchV2/resources/lang/zh/message.php
+++ cpresearchV2/resources/lang/zh/message.php
@@ -37,4 +37,86 @@ return [
 
 // cpresearchV2\resources\views\welcome.blade.php
     'welcome_message' => '欢迎来到计算机科学系的研究数据管理系统',
+
+
+
+
+//=========================Mean==============================
+
+
+
+    // cpresearchV2/resources/views/research_g.blade.php
+    'Research_Group' => '研究小组',
+    'Laboratory_Supervisor' => '实验室主管',
+    // 'Expertise' => '专业领域',
+    'Add_Expertise' => '添加专业领域',
+    // 'Course' => '课程',
+    'Research_Information_Management_System' => '研究信息管理系统',
+    // 'Add_Course' => '添加课程',
+
+    // cpresearchV2/resources/views/programs/index.blade.php
+    'Add_New_Program' => '添加新课程',
+    'Education_Level' => '教育水平',
+    'Academic_Program' => '学术课程',
+    'Name_TH' => '名称 (泰文)',
+    'Name_EN' => '名称 (英文)',
+    'Submit' => '提交',
+    'Cancle' => '取消',
+
+    // cpresearchV2/resources/views/dashboards/users/profile.blade.php
+    'Name' => '姓名',
+    'Expert_Name' => '专家姓名',
+
+    // cpresearchV2/resources/views/departments/create.blade.php
+    'Department_Name_TH' => '系名称 (泰文)',
+    'Department_Name_EN' => '系名称 (英文)',
+    'Create_Department' => '创建系',
+    'Departments' => '系',
+
+    // cpresearchV2/resources/views/books/create.blade.php
+    'Add_Book' => '添加书籍',
+    'Book_Name' => '书名',
+    'Enter_Book_Details' => '输入书籍详细信息',
+    'Place_Of_Publication' => '出版地点',
+    'Year_(A.D.)' => '出版年份 (公元)',
+    'Number_Of_Pages' => '页数',
+
+    // cpresearchV2/resources/views/research_groups/create.blade.php
+    'Create_Research_Group' => '创建研究小组',
+    'Edit_Research_Group_Details' => '编辑研究小组详情',
+    'Research_Group_Name_TH' => '研究小组名称 (泰文)',
+    'Research_Group_Name_EN' => '研究小组名称 (英文)',
+    'Research_Group_Description_TH' => '研究小组描述 (泰文)',
+    'Research_Group_Description_EN' => '研究小组描述 (英文)',
+    'Image' => '图片',
+    'Research_Group_Leader' => '研究小组负责人',
+    'Research_Group_Members' => '研究小组成员',
+    'Back' => '返回',
+
+    // cpresearchV2/resources/views/users/create.blade.php
+    'Add_User' => '添加用户',
+    'Add_User_Details' => '编辑用户详情',
+    'First_Name_TH' => '名字 (泰文)',
+    'Last_Name_TH' => '姓氏 (泰文)',
+    'First_Name_EN' => '名字 (英文)',
+    'Last_Name_EN' => '姓氏 (英文)',
+    'Email' => '电子邮件',
+    'Password' => '密码',
+    'Confirm_Password' => '确认密码',
+    'Role' => '角色',
+    'Department' => '系',
+    'Program' => '课程',
+    'Select_Subcategory' => '选择子类别',
+    'Scholar_ID_(Optional)' => '学者编号 (可选)',
+
+    // cpresearchV2/resources/views/users/import.blade.php
+    'Import' => '导入 Excel, CSV 文件',
+    'Choose_File' => '选择文件',
+
+
+
+
+
+//=========================Mean==============================
+
 ];
