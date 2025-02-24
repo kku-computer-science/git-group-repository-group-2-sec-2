d5e62c501d527483c3b5e6cbb9965b942fb8ea12
Museey <musee.hyper@gmail.com>
2025-02-24 15:34:01 +0700

    50% done
    User page - import page
    User page - create page
    Permission page - create page
    Manage Patent - create page
    Manage Fund - create page

--- cpresearchV2/resources/views/dashboards/users/layouts/user-dash-layout.blade.php
+++ cpresearchV2/resources/views/dashboards/users/layouts/user-dash-layout.blade.php
@@ -83,6 +83,23 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             <input type="search" class="form-control" placeholder="Search Here" title="Search here">
                         </form>
                     </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            <span
+                                class="flag-icon flag-icon-{{Config::get('languages')[App::getLocale()]['flag-icon']}}"></span>
+                            {{ Config::get('languages')[App::getLocale()]['display'] }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            @foreach (Config::get('languages') as $lang => $language)
+                            @if ($lang != App::getLocale())
+                            <a class="dropdown-item" href="{{ route('langswitch', $lang) }}"><span
+                                    class="flag-icon flag-icon-{{$language['flag-icon']}}"></span>
+                                {{$language['display']}}</a>
+                            @endif
+                            @endforeach
+                        </div>
+                    </li>
                     <!-- <li class="nav-item dropdown">
                         <a class="nav-link count-indicator" id="countDropdown" href="#" data-bs-toggle="dropdown"
                             aria-expanded="false">
--- cpresearchV2/resources/views/researchprofiles.blade.php
+++ cpresearchV2/resources/views/researchprofiles.blade.php
@@ -53,7 +53,7 @@
                         @else
                         <h6 class="card-text"><b>{{$res->fname_en}} {{$res->lname_en}}</b>
                             @endif</h6>
-                        <h6 class="card-text1"><b>{{$res->academic_ranks_en}}</b></h6>
+                            <h6 class="card-text1"><b>{{$res->{'academic_ranks_' . app()->getLocale()} }}</b></h6>
                         <!-- <h6 class="card-text1">Department of {{$res->program->program_name_en}}</h6> -->
                         <!-- <h6 class="card-text1">College of Computing</h6>
                     <h6 class="card-text1">Khon Kaen University</h6> -->
@@ -136,7 +136,7 @@
 
     <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Summary</button>
+            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">{{ trans('message.Summary') }}</button>
         </li>
         <li class="nav-item" role="presentation">
             <button class="nav-link" id="scopus-tab" data-bs-toggle="tab" data-bs-target="#scopus" type="button" role="tab" aria-controls="scopus" aria-selected="false">SCOPUS</button>
@@ -148,10 +148,10 @@
             <button class="nav-link" id="tci-tab" data-bs-toggle="tab" data-bs-target="#tci" type="button" role="tab" aria-controls="tci" aria-selected="false">TCI</button>
         </li>
         <li class="nav-item" role="presentation">
-            <button class="nav-link" id="book-tab" data-bs-toggle="tab" data-bs-target="#book" type="button" role="tab" aria-controls="book" aria-selected="false">หนังสือ</button>
+            <button class="nav-link" id="book-tab" data-bs-toggle="tab" data-bs-target="#book" type="button" role="tab" aria-controls="book" aria-selected="false">{{ trans('message.Book') }}</button>
         </li>
         <li class="nav-item" role="presentation">
-            <button class="nav-link" id="patent-tab" data-bs-toggle="tab" data-bs-target="#patent" type="button" role="tab" aria-controls="patent" aria-selected="false">ผลงานวิชาการด้านอื่นๆ</button>
+            <button class="nav-link" id="patent-tab" data-bs-toggle="tab" data-bs-target="#patent" type="button" role="tab" aria-controls="patent" aria-selected="false">{{ trans('message.Others') }}</button>
         </li>
         <li class="nav-item" role="presentation">
             <button class="nav-link" id="scholar-tab" data-bs-toggle="tab" data-bs-target="#scholar" type="button" role="tab" aria-controls="scholar" aria-selected="false">Scholar</button>
@@ -162,7 +162,7 @@
 
         <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
             <div class="tab-content" style="padding-bottom: 20px;">
-                <a class="btn btn-success" href="{{ route('excel', ['id' => $res->id]) }}" target="_blank">Export To Excel</a>
+                <a class="btn btn-success" href="{{ route('excel', ['id' => $res->id]) }}" target="_blank">{{ trans('message.Export to Excel')}}</a>
             </div>
             <table id="example1" class="table table-striped" style="width:100%">
                 <thead>
@@ -170,16 +170,16 @@
                         <th><a href="{{ route('excel', ['id' => $res->id]) }}" target="_blank">#Export</a></td>
                     </tr> -->
                     <tr>
-                        <th>No.</th>
-                        <th>Year</th>
-                        <th>Paper Name</th>
-                        <th>Author</th>
-                        <th>Document Type</th>
-                        <th>Page</th>
-                        <th>Journals/Transactions</th>
-                        <th>Ciations</th>
-                        <th>Doi</th>
-                        <th>Source</th>
+                        <th>{{ trans('message.No.') }}</th>
+                        <th>{{ trans('message.Year') }}</th>
+                        <th>{{ trans('message.Paper Name') }}</th>
+                        <th>{{ trans('message.Author') }}</th>
+                        <th>{{ trans('message.Document Type') }}</th>
+                        <th>{{ trans('message.Page') }}</th>
+                        <th>{{ trans('message.Journals/Transactions') }}</th>
+                        <th>{{ trans('message.Citations') }}</th>
+                        <th>{{ trans('message.Doi') }}</th>
+                        <th>{{ trans('message.Source') }}</th>
                     </tr>
                 </thead>
 
@@ -199,7 +199,8 @@
                             @foreach ($paper->teacher as $author)
                             <span >
                                 <a href="{{ route('detail',Crypt::encrypt($author->id))}}">
-                                    <teacher>{{$author -> fname_en}} {{$author -> lname_en}}</teacher></a>
+                                <teacher>{{ $author->{'fname_' . app()->getLocale()} }} {{ $author->{'lname_' . app()->getLocale()} }}</teacher>
+                                </a>
                             </span>
                             @endforeach
                         </td>
@@ -228,15 +229,15 @@
             <table id="example2" class="table table-striped" style="width:100%">
                 <thead>
                     <tr>
-                        <th>No.</th>
-                        <th>Year</th>
-                        <th style="width:90%;">Paper Name</th>
-                        <th>Author</th>
-                        <th>Document Type</th>
-                        <th style="width:100%;">Page</th>
-                        <th>Journals/Transactions</th>
-                        <th>Ciations</th>
-                        <th>Doi</th>
+                        <th>{{ trans('message.No.') }}</th>
+                        <th>{{ trans('message.Year') }}</th>
+                        <th style="width:90%">{{ trans('message.Paper Name') }}</th>
+                        <th>{{ trans('message.Author') }}</th>
+                        <th>{{ trans('message.Document Type') }}</th>
+                        <th style="width:100%">{{ trans('message.Page') }}</th>
+                        <th>{{ trans('message.Journals/Transactions') }}</th>
+                        <th>{{ trans('message.Citations') }}</th>
+                        <th>{{ trans('message.Doi') }}</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -279,15 +280,15 @@
             <table id="example3" class="table table-striped" style="width:100%">
                 <thead>
                     <tr>
-                        <th>No.</th>
-                        <th>Year</th>
-                        <th style="width:90%;">Paper Name</th>
-                        <th>Author</th>
-                        <th>Document Type</th>
-                        <th style="width:100%;">Page</th>
-                        <th>Journals/Transactions</th>
-                        <th>Ciations</th>
-                        <th>Doi</th>
+                    <th>{{ trans('message.No.') }}</th>
+                        <th>{{ trans('message.Year') }}</th>
+                        <th style="width:90%">{{ trans('message.Paper Name') }}</th>
+                        <th>{{ trans('message.Author') }}</th>
+                        <th>{{ trans('message.Document Type') }}</th>
+                        <th style="width:100%">{{ trans('message.Page') }}</th>
+                        <th>{{ trans('message.Journals/Transactions') }}</th>
+                        <th>{{ trans('message.Citations') }}</th>
+                        <th>{{ trans('message.Doi') }}</th>
                     </tr>
                 </thead>
 
@@ -331,15 +332,15 @@
             <table id="example4" class="table table-striped" style="width:100%">
                 <thead>
                     <tr>
-                        <th>No.</th>
-                        <th>Year</th>
-                        <th style="width:90%;">Paper Name</th>
-                        <th>Author</th>
-                        <th>Document Type</th>
-                        <th style="width:100%;">Page</th>
-                        <th>Journals/Transactions</th>
-                        <th>Ciations</th>
-                        <th>Doi</th>
+                    <th>{{ trans('message.No.') }}</th>
+                        <th>{{ trans('message.Year') }}</th>
+                        <th style="width:90%">{{ trans('message.Paper Name') }}</th>
+                        <th>{{ trans('message.Author') }}</th>
+                        <th>{{ trans('message.Document Type') }}</th>
+                        <th style="width:100%">{{ trans('message.Page') }}</th>
+                        <th>{{ trans('message.Journals/Transactions') }}</th>
+                        <th>{{ trans('message.Citations') }}</th>
+                        <th>{{ trans('message.Doi') }}</th>
                     </tr>
                 </thead>
 
@@ -379,12 +380,12 @@
             <table id="example5" class="table table-striped" style="width:100%">
                 <thead>
                     <tr>
-                        <th scope="col">Number</th>
-                        <th scope="col">Year</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Author</th>
-                        <th scope="col">สถานที่พิมพ์</th>
-                        <th scope="col">Page</th>
+                        <th scope="col">{{ trans('message.No.') }}</th>
+                        <th scope="col">{{ trans('message.Year') }}</th>
+                        <th scope="col">{{ trans('message.BookName') }}</th>
+                        <th scope="col">{{ trans('message.Author') }}</th>
+                        <th scope="col">{{ trans('message.Publication Place') }}</th>
+                        <th scope="col">{{ trans('message.Page') }}</th>
 
                     </tr>
                 </thead>
@@ -421,12 +422,12 @@
             <table id="example6" class="table table-striped" style="width:100%">
                 <thead>
                     <tr>
-                        <th scope="col">Number</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Author</th>
-                        <th scope="col">ประเภท</th>
-                        <th scope="col">หมายเลขทะเบียน</th>
-                        <th scope="col">วันที่จดทะเบียน</th>
+                        <th scope="col">{{ trans('message.No.') }}</th>
+                        <th scope="col">{{ trans('message.Name') }}</th>
+                        <th scope="col">{{ trans('message.Author') }}</th>
+                        <th scope="col">{{ trans('message.Type') }}</th>
+                        <th scope="col">{{ trans('message.Registration Number')}}</th>
+                        <th scope="col">{{ trans('message.Registration Date')}}</th>
 
                     </tr>
                 </thead>
@@ -465,15 +466,14 @@
     <table id="example7" class="table table-striped" style="width:100%">
         <thead>
             <tr>
-                <th>No.</th>
-                <th>Year</th>
-                <th>Paper Name</th>
-                <th>Author</th>
-                <th>Document Type</th>
-                <th>Page</th>
-                <th>Journals/Transactions</th>
-                <th>Citations</th>
-                <th>Doi</th>
+                <th>{{ trans('message.Year') }}</th>
+                        <th style="width:90%">{{ trans('message.Paper Name') }}</th>
+                        <th>{{ trans('message.Author') }}</th>
+                        <th>{{ trans('message.Document Type') }}</th>
+                        <th style="width:100%">{{ trans('message.Page') }}</th>
+                        <th>{{ trans('message.Journals/Transactions') }}</th>
+                        <th>{{ trans('message.Citations') }}</th>
+                        <th>{{ trans('message.Doi') }}</th>
             </tr>
         </thead>
         <tbody>
@@ -525,28 +525,29 @@
 <script>
     $(document).ready(function() {
 
-        var table1 = $('#example1').DataTable({
-            responsive: true,
-        });
+        var MultiLang = {
+            "sProcessing": "{{ trans('message.Processing...') }}",
+            "sLengthMenu": "{{ trans('message.Showing') }} _MENU_ {{ trans('message.entries') }}",
+            "sZeroRecords": "{{ trans('message.No data available in table') }}",
+            "sInfo": "{{ trans('message.Showing') }} _START_ {{ trans('message.to') }} _END_ {{ trans('message.of') }} _TOTAL_ {{ trans('message.entries') }}",
+            "sInfoEmpty": "{{ trans('message.Showing') }} 0 {{ trans('message.to') }} 0 {{ trans('message.of') }} 0 {{ trans('message.entries') }}",
+            "sInfoFiltered": "({{ trans('message.filtered from') }} _MAX_ {{ trans('message.entries') }})",
+            "sSearch": "{{ trans('message.Search') }}:",
+            "oPaginate": {
+                "sFirst": "{{ trans('message.First Page') }}",
+                "sPrevious": "{{ trans('message.Previous Page') }}",
+                "sNext": "{{ trans('message.Next Page') }}",
+                "sLast": "{{ trans('message.Last Page') }}"
+            }
+        };
 
-        var table2 = $('#example2').DataTable({
-            responsive: true,
-        });
-        var table3 = $('#example3').DataTable({
-            responsive: true,
-        });
-        var table4 = $('#example4').DataTable({
-            responsive: true,
-        });
-        var table5 = $('#example5').DataTable({
-            responsive: true,
-        });
-        var table6 = $('#example6').DataTable({
-            responsive: true,
-        });
-        var table7 = $('#example7').DataTable({
-    responsive: true,
-        });
+        var table1 = $('#example1').DataTable({ responsive: true, language: MultiLang });
+        var table2 = $('#example2').DataTable({ responsive: true, language: MultiLang });
+        var table3 = $('#example3').DataTable({ responsive: true, language: MultiLang });
+        var table4 = $('#example4').DataTable({ responsive: true, language: MultiLang });
+        var table5 = $('#example5').DataTable({ responsive: true, language: MultiLang });
+        var table6 = $('#example6').DataTable({ responsive: true, language: MultiLang });
+        var table7 = $('#example7').DataTable({ responsive: true, language: MultiLang });
 
 
         $('button[data-bs-toggle="tab"]').on('shown.bs.tab', function(event) {
@@ -735,7 +736,7 @@
         //$("#scopus").append('data-to="100"');
         document.getElementById("all").innerHTML += `   
                 <h2 class="timer count-title count-number" data-to="${sum}" data-speed="1500"></h2>
-                <p class="count-text ">SUMMARY</p>`
+                <p class="count-text ">{{ trans('message.Summary') }}</p>`
 
         document.getElementById("scopus_sum").innerHTML += `   
                 <h2 class="timer count-title count-number" data-to="${sumsco}" data-speed="1500"></h2>
@@ -853,15 +854,5 @@
         }
     });
 </script>
-<!-- <script>
-    // get the p element
-    $(document).ready(function() {
-    const a = document.getElementById('authtd');
-    console.log(a.text)
-    const myArray =  a.text.toString().split(" ");
-    console.log(myArray)
-    document.getElementById("authtd").innerHTML = "name :"+ myArray;
-    
-});
-</script> -->
+
 @endsection
