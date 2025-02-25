30ea04220c13c983817c9cd04d63bac29365105f
bumb1gbrain <giszlozz@gmail.com>
2025-02-25 21:24:37 +0700

    research project (edit page)

--- cpresearchV2/resources/views/research_projects/edit.blade.php
+++ cpresearchV2/resources/views/research_projects/edit.blade.php
@@ -14,7 +14,7 @@
 <div class="container">
     @if ($errors->any())
     <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <strong>{{ trans('message.Whoops') }}</strong> {{ trans('message.There were some problems with your input') }}.<br><br>
         <ul>
             @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
@@ -30,73 +30,73 @@
                 @csrf
                 @method('PUT')
                 <div class="form-group row">
-                    <p class="col-sm-3 "><b>ชื่อโครงการ</b></p>
+                    <p class="col-sm-3 "><b>{{ trans('message.Project Name') }}</b></p>
                     <div class="col-sm-8">
                         <textarea name="project_name" value="{{ $researchProject->project_name }}" class="form-control" style="height:90px">{{ $researchProject->project_name }}</textarea>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3 "><b>วันเริ่มต้นโครงการ</b></p>
+                    <p class="col-sm-3 "><b>{{ trans('message.Start Date') }}</b></p>
                     <div class="col-sm-4">
                         <input type="date" name="project_start" id="Project_start" value="{{ $researchProject->project_start }}" class="form-control">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3 "><b>วันสิ้นสุดโครงการ</b></p>
+                    <p class="col-sm-3 "><b>{{ trans('message.End Date') }}</b></p>
                     <div class="col-sm-4">
                         <input type="date" name="project_end" id="Project_end" value="{{ $researchProject->project_end }}" class="form-control">
                     </div>
                 </div>
                 <div class="form-group row mt-2">
-                    <p for="exampleInputfund_details" class="col-sm-3"><b>เลือกทุน</b></p>
+                    <p for="exampleInputfund_details" class="col-sm-3"><b>{{ trans('message.Select Fund') }}</b></p>
                     <div class="col-sm-9">
                         <select id='fund' style='width: 200px;' class="custom-select my-select" name="fund">
-                            <option value='' disabled selected>เลือกทุนวิจัย</option>@foreach($funds as $f)<option value="{{ $f->id }}" {{ $f->fund_name == $researchProject->fund->fund_name ? 'selected' : '' }}>{{ $f->fund_name }}</option>
+                            <option value='' disabled selected>{{ trans('message.Select Fund') }}</option>@foreach($funds as $f)<option value="{{ $f->id }}" {{ $f->fund_name == $researchProject->fund->fund_name ? 'selected' : '' }}>{{ $f->fund_name }}</option>
                             @endforeach
                         </select>
                     </div>
                 </div>
                 <div class="form-group row mt-2">
-                    <p class="col-sm-3 "><b>ปีที่ยื่น (ค.ศ.)</b></p>
+                    <p class="col-sm-3 "><b>{{ trans('message.Project Year') }}</b></p>
                     <div class="col-sm-8">
                         <input type="year" name="project_year" class="form-control" placeholder="ปี" value="{{$researchProject->project_year}}">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3 "><b>งบประมาณ</b></p>
+                    <p class="col-sm-3 "><b>{{ trans('message.Budget') }}</b></p>
                     <div class="col-sm-4">
                         <input type="number" name="budget" value="{{ $researchProject->budget }}" class="form-control">
                     </div>
                 </div>
                 <div class="form-group row mt-2">
-                    <p class="col-sm-3 "><b>หน่วยงานที่รับผิดชอบ</b></p>
+                    <p class="col-sm-3 "><b>{{ trans('message.Responsible Department') }}</b></p>
                     <div class="col-sm-3">
                         <select id='dep' style='width: 200px;' class="custom-select my-select" name="responsible_department">
-                            <option value=''>เลือกสาขาวิชา</option>@foreach($deps as $dep)<option value="{{ $dep->department_name_th }}" {{ $dep->department_name_th == $researchProject->responsible_department ? 'selected' : '' }}>{{ $dep->department_name_th }}</option>@endforeach
+                            <option value=''>{{ trans('message.Select Department') }}</option>@foreach($deps as $dep)<option value="{{ $dep->department_name_th }}" {{ $dep->department_name_th == $researchProject->responsible_department ? 'selected' : '' }}>{{ $dep->department_name_th }}</option>@endforeach
                         </select>
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    <p class="col-sm-3 "><b>รายละเอียดโครงการวิจัย</b></p>
+                    <p class="col-sm-3 "><b>{{ trans('message.Project Details') }}</b></p>
                     <div class="col-sm-8">
                         <textarea name="note" class="form-control" style="height:90px">{{ $researchProject->note }}</textarea>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3 "><b>สถานะ</b></p>
+                    <p class="col-sm-3 "><b>{{ trans('message.Status') }}</b></p>
                     <div class="col-sm-8">
                         <select id='status' class="custom-select my-select" style='width: 200px;' name="status">
-                            <option value="1" {{ 1 == $researchProject->status ? 'selected' : '' }}>ยื่นขอ</option>
-                            <option value="2" {{ 2 == $researchProject->status ? 'selected' : '' }}>ดำเนินการ</option>
-                            <option value="3" {{ 3 == $researchProject->status ? 'selected' : '' }}>ปิดโครงการ</option>
+                            <option value="1" {{ 1 == $researchProject->status ? 'selected' : '' }}>{{ trans('message.Submitted') }}</option>
+                            <option value="2" {{ 2 == $researchProject->status ? 'selected' : '' }}>{{ trans('message.In Progress') }}</option>
+                            <option value="3" {{ 3 == $researchProject->status ? 'selected' : '' }}>{{ trans('message.Closed') }}</option>
                         </select>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <table class="table">
                         <tr>
-                            <th>ผู้รับผิดชอบโครงการ</th>
+                            <th>{{ trans('message.Project Leader') }}</th>
                         <tr>
                             <td>
                                 <select id='head0' style='width: 200px;' name="head">
@@ -128,29 +128,37 @@
                 <div class="col-xs-12 col-sm-12 col-md-12 mb-3">
                     <table class="table " id="dynamicAddRemove">
                         <tr>
-                            <th width="522.98px">ผู้รับผิดชอบโครงการ (ร่วม) ภายใน</th>
+                            <th width="522.98px">{{ trans('message.Internal Co-Leader') }}</th>
                             <th><button type="button" name="add" id="add-btn2" class="btn btn-success btn-sm add"><i class="mdi mdi-plus"></i></button></th>
                         </tr>
                     </table>
                 </div>
                 <div class="form-group row">
-                    <label for="exampleInputpaper_author" class="col-sm-3 col-form-label">ผู้รับผิดชอบโครงการ (ร่วม) ภายนอก</label>
+                    <label for="exampleInputpaper_author" class="col-sm-3 col-form-label">{{ trans('message.External Co-Leader') }}</label>
                     <div class="col-sm-9">
                         <div class="table-responsive">
-                            <table class="table table-bordered w-75" id="dynamic_field">
-
+                            <table class="table table-hover small-text" id="tb">
+                                <tr class="tr-header">
+                                    <th>{{ trans('message.Title') }}</th>
+                                    <th>{{ trans('message.First Name') }}</th>
+                                    <th>{{ trans('message.Last Name') }}</th>
+                                    <!-- <th>Email Id</th> -->
+                                    <!-- <button type="button" name="add" id="add" class="btn btn-success btn-sm"><i class="mdi mdi-plus"></i></button> -->
+                                    <th><a href="javascript:void(0);" style="font-size:18px;" id="addMore2" title="{{ trans('message.Add More Person') }}"><i class="mdi mdi-plus"></i></span></a></th>
                                 <tr>
-                                    <td><button type="button" name="add" id="add" class="btn btn-success btn-sm"><i class="mdi mdi-plus"></i></button>
-                                    </td>
+                                    <td><input type="text" name="title_name[]" class="form-control" placeholder="{{ trans('message.Title') }}"></td>
+                                    <td><input type="text" name="fname[]" class="form-control" placeholder="{{ trans('message.First Name') }}"></td>
+                                    <td><input type="text" name="lname[]" class="form-control" placeholder="{{ trans('message.Last Name') }}"></td>
+                                    <!-- <td><input type="text" name="emailid[]" class="form-control"></td> -->
+                                    <td><a href='javascript:void(0);' class='remove'><span><i class="mdi mdi-minus"></span></a></td>
                                 </tr>
-
                             </table>
                             <!-- <input type="button" name="submit" id="submit" class="btn btn-info" value="Submit" /> -->
                         </div>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary mt-5">Submit</button>
-                <a class="btn btn-light mt-5" href="{{ route('researchProjects.index') }}"> Back</a>
+                <button type="submit" class="btn btn-primary mt-5">{{trans('message.submit_button')}}</button>
+                <a class="btn btn-light mt-5" href="{{ route('researchProjects.index') }}">{{trans('message.cancel_button')}}</a>
             </form>
         </div>
     </div>
@@ -158,99 +166,105 @@
 @stop
 @section('javascript')
 <script>
-$(document).ready(function () {
-    $("#head0").select2();
+    $(document).ready(function() {
+        $("#head0").select2();
 
-    function updateAvailableOptions() {
-        let headUserId = $("#head0").val();
-        let selectedUsers = new Set();
+        function updateAvailableOptions() {
+            let headUserId = $("#head0").val();
+            let selectedUsers = new Set();
 
-        // ดึงค่าผู้ที่ถูกเลือกไปแล้ว
-        $("select[name^='moreFields']").each(function () {
-            let val = $(this).val();
-            if (val) selectedUsers.add(val);
-        });
-
-        // ปิดการเลือกชื่อตามที่ถูกเลือกไปแล้ว
-        $("select[name^='moreFields']").each(function () {
-            let currentValue = $(this).val();
-            $(this).find("option").each(function () {
-                let optionValue = $(this).val();
-
-                if (optionValue && (selectedUsers.has(optionValue) && optionValue !== currentValue || optionValue === headUserId)) {
-                    $(this).prop("disabled", true);
-                } else {
-                    $(this).prop("disabled", false);
-                }
+            // ดึงค่าผู้ที่ถูกเลือกไปแล้ว
+            $("select[name^='moreFields']").each(function() {
+                let val = $(this).val();
+                if (val) selectedUsers.add(val);
             });
+
+            // ปิดการเลือกชื่อตามที่ถูกเลือกไปแล้ว
+            $("select[name^='moreFields']").each(function() {
+                let currentValue = $(this).val();
+                $(this).find("option").each(function() {
+                    let optionValue = $(this).val();
+
+                    if (optionValue && (selectedUsers.has(optionValue) && optionValue !== currentValue || optionValue === headUserId)) {
+                        $(this).prop("disabled", true);
+                    } else {
+                        $(this).prop("disabled", false);
+                    }
+                });
+            });
+        }
+
+        $(document).on("change", "select[name^='moreFields']", function() {
+            updateAvailableOptions();
         });
-    }
 
-    $(document).on("change", "select[name^='moreFields']", function () {
-        updateAvailableOptions();
-    });
-
-    var i = 0;
-    $("#add-btn2").click(function () {
-        ++i;
-        var newRow = `<tr>
+        var i = 0;
+        $("#add-btn2").click(function() {
+            ++i;
+            var newRow = `<tr>
                         <td>
                             <select id="selUser${i}" name="moreFields[${i}][userid]" class="form-control selectUser" style="width: 200px;">
-                                <option value="">Select User</option>
+                                <option value="">{{ trans('message.Select User') }}</option>
                                 @foreach($users as $user)
-                                    <option value="{{ $user->id }}">{{ $user->fname_th }} {{ $user->lname_th }}</option>
+                                    @php
+                                        $locale = app()->getLocale();
+                                        $fname = $user->fname_en;
+                                        $lname = $user->lname_en;
+                                        if ($locale == 'th') {
+                                            $fname = $user->fname_th;
+                                            $lname = $user->lname_th;
+                                        }
+                                    @endphp
+                                    <option value="{{ $user->id }}">{{ $fname }} {{ $lname }}</option>
                                 @endforeach
                             </select>
                         </td>
                         <td><button type="button" class="btn btn-danger btn-sm remove-tr"><i class="mdi mdi-minus"></i></button></td>
                     </tr>`;
 
-        $("#dynamicAddRemove").append(newRow);
-        $("#selUser" + i).select2();
+            $("#dynamicAddRemove").append(newRow);
+            $("#selUser" + i).select2();
+            updateAvailableOptions();
+        });
+
+        $(document).on("click", ".remove-tr", function() {
+            $(this).parents("tr").remove();
+            updateAvailableOptions();
+        });
+
         updateAvailableOptions();
     });
-
-    $(document).on("click", ".remove-tr", function () {
-        $(this).parents("tr").remove();
-        updateAvailableOptions();
-    });
-
-    updateAvailableOptions();
-});
-
 </script>
 <script>
     $(document).ready(function() {
-        var outsider = <?php echo $researchProject->outsider; ?>;
+        var outsider = <?php echo json_encode($researchProject->outsider); ?>;
 
         var postURL = "<?php echo url('addmore'); ?>";
         var i = 0;
-        //console.log(patent)
 
         for (i = 0; i < outsider.length; i++) {
-            value = "'+ obj.title_name +'"
-            //console.log(patent);
             var obj = outsider[i];
             $("#dynamic_field").append('<tr id="row' + i +
-                '" class="dynamic-added"><td><p>ตำแหน่งหรือคำนำหน้า :</p><input type="text" name="title_name[]" value="' + obj.title_name + '" placeholder="ตำแหน่งหรือคำนำหน้า" style="width: 200px;" class="form-control name_list" /><br><p>ชื่อ :</p><input type="text" name="fname[]" value="' + obj.fname + '" placeholder="ชื่อ" style="width: 300px;" class="form-control name_list" /><br><p>นามสกุล :</p><input type="text" name="lname[]" value="' + obj.lname + '" placeholder="นามสกุล" style="width: 300px;" class="form-control name_list" /></td><td><button type="button" name="remove" id="' +
+                '" class="dynamic-added"><td><p>{{ trans('
+                message.Title ') }} :</p><input type="text" name="title_name[]" value="' + obj.title_name + '" placeholder="{{ trans('
+                message.Title ') }}" style="width: 200px;" class="form-control name_list" /><br><p>{{ trans('
+                message.First Name ') }} :</p><input type="text" name="fname[]" value="' + obj.fname + '" placeholder="{{ trans('
+                message.First Name ') }}" style="width: 300px;" class="form-control name_list" /><br><p>{{ trans('
+                message.Last Name ') }} :</p><input type="text" name="lname[]" value="' + obj.lname + '" placeholder="{{ trans('
+                message.Last Name ') }}" style="width: 300px;" class="form-control name_list" /></td><td><button type="button" name="remove" id="' +
                 i + '" class="btn btn-danger btn-sm btn_remove"><i class="mdi mdi-minus"></i></button></td></tr>');
-            //document.getElementById("selUser" + i).value = obj.id;
-            //console.log(obj.author_fname)
-            // let doc=document.getElementById("row" + i)
-            // doc.setAttribute('fname','aaa');
-            // doc.setAttribute('lname','bbb');
-            //document.getElementById("row" + i).value = obj.author_lname;
-            //document.getAttribute("lname").value = obj.author_lname;
-            //$("#selUser" + i).select2()
-
-
-            //document.getElementById("#dynamicAddRemove").value = "10";
         }
 
         $('#add').click(function() {
             i++;
             $('#dynamic_field').append('<tr id="row' + i +
-                '" class="dynamic-added"><td><p>ตำแหน่งหรือคำนำหน้า :</p><input type="text" name="title_name[]" placeholder="ตำแหน่งหรือคำนำหน้า" style="width: 200px;" class="form-control name_list" /><br><p>ชื่อ :</p><input type="text" name="fname[]" placeholder="ชื่อ" style="width: 300px;" class="form-control name_list" /><br><p>นามสกุล :</p><input type="text" name="lname[]" placeholder="นามสกุล" style="width: 300px;" class="form-control name_list" /></td><td><button type="button" name="remove" id="' +
+                '" class="dynamic-added"><td><p>{{ trans('
+                message.Title ') }} :</p><input type="text" name="title_name[]" placeholder="{{ trans('
+                message.Title ') }}" style="width: 200px;" class="form-control name_list" /><br><p>{{ trans('
+                message.First Name ') }} :</p><input type="text" name="fname[]" placeholder="{{ trans('
+                message.First Name ') }}" style="width: 300px;" class="form-control name_list" /><br><p>{{ trans('
+                message.Last Name ') }} :</p><input type="text" name="lname[]" placeholder="{{ trans('
+                message.Last Name ') }}" style="width: 300px;" class="form-control name_list" /></td><td><button type="button" name="remove" id="' +
                 i + '" class="btn btn-danger btn-sm btn_remove"><i class="mdi mdi-minus"></i></button></td></tr>');
         });
 
@@ -269,7 +283,7 @@ $(document).ready(function () {
         function validateDates() {
             if (projectStart.value && projectEnd.value) {
                 if (projectEnd.value < projectStart.value) {
-                    alert("เลือกวันที่สิ้นสุดก่อนวันที่เริ่มต้นไม่ได้!");
+                    alert("{{ trans('message.Invalid Date Range') }}");
                     projectEnd.value = ""; // รีเซ็ตค่าของวันที่สิ้นสุด
                 }
             }
@@ -287,5 +301,4 @@ $(document).ready(function () {
         });
     });
 </script>
-
 @stop
